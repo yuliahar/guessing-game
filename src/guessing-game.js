2ed8d5bea77f91cb708c5e5035095fb752a1b06e
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2017-01-30 18:56:42 +0300

    Initial commit.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.average();
     }
 
     lower() {
-
+        this.max = this.average();
     }
 
     greater() {
+        this.min = this.average();
+    }
 
+    average() {
+        return Math.ceil((this.max + this.min) / 2);
     }
 }
 
